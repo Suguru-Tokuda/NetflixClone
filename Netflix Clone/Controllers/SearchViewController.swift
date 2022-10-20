635ab581047b9fe61423ca1566601976a1f2f19f
Suguru Tokuda <suguru.tokuda@gmail.com>
2022-10-20 10:25:23 -0500

    Added table view

---
+++ Netflix Clone/Controllers/SearchViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SearchViewController.swift
+//  Netflix Clone
+//
+//  Created by Suguru on 10/19/22.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
---
+++ Netflix Clone/Controllers/UpcomingViewController.swift
@@ -0,0 +1,15 @@
+//
+//  UpcomingViewController.swift
+//  Netflix Clone
+//
+//  Created by Suguru on 10/19/22.
+//
+
+import UIKit
+
+class UpcomingViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
